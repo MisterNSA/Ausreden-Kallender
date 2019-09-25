7fd07aa739dc6c0da4bfd28eeabbd1e8b9584112
MisterNSA <tobiasweber1029@gmail.com>
2019-09-25 15:51:43 +0200

    Update Ausredenkallender.py

--- Ausredenkallender.py
+++ Ausredenkallender.py
@@ -2,6 +2,66 @@
 # Ersteller: Tobias Weber
 # Version 0.0 17.08.1
 
+
+'''
+-Zufallsgenerator erstellen, der zufällig eine Ausrede auswählt (Am besten eigene Funktion "Zufallsgenerator", der Wert per return zurück gibt)
+-
+'''
 import random
 
-def Ausredenkalender()
+#def Ausredenkalender():
+
+
+
+
+
+
+
+
+
+
+def Zufallsgenerator():
+
+    Ausreden_Liste = [
+    "Koronaler Massenauswurf",
+    "Zu niedrige Tacktfrequenz",
+    "Zu hohe Speicherauslastung",
+    "Problem wegen nicht eingestellter Interrupts an PCI",
+    "Fehlerhafte Sektoren im NAS-System",
+    "Die neuen Treiber haben Kompatibilitätsprobleme",
+    "Die Speicherresidenten Programme haben sich gegenseitig abgeschossen",
+    "Das verträgt sich nicht mit der Terminalemulation",
+    "Anti-Virus-Programm klassifiziert Windows Update als Virus",
+    "Platte am Server wurde falsch gepolt angeschlossen und dreht jetzt falsch herum",
+    "Momentan telefonieren zu viele Leute, weshalb das Netzwerk ausgelastet ist",
+    "Aufgrund zu hoher Stromrechnungen laufen die Geräte im Energiesparmodus und sind dadurch langsamer",
+    "Ständige Telefonanrufe an die Hotline verhindern, dass Schäden behoben werden können" ,
+    "Auf Ihrem Rechner ist kein RTFM-Device installiert",
+    "Die Leistung wird für das aktuelle Systembackup benötigt",
+    "Auf ihrem Rechner war /dev/brain nach /dev/null gelinkt",
+    "Die Teritäre Hintergrundstrahlung ist zu hoch für die neuen (RAMs,ROMs,CPUs,etc.)",
+    "Auf allen Rechnern laufen Service-Pack-Updates",
+    "Borg-Prozess im Linux-Cluster assimiliert alle Systemprozesse",
+    "Beim Dual-Prozessor-Server streiten die CPUs um internen Cache-Speicher",
+    "Ihr Computer hat die Bits nicht zurückgegeben, die er vom Internet bekommen hat",
+    "Fehler 40 sorgt für Fehlerhafte Prozessausführung ",
+    "Die CPU braucht eine Rekalibrierung",
+    "Schleife in der Schleife des redundanten Loopback",
+    "Wir haben nicht genug Lizenzen",
+    "Der Prozess ist nicht ISO 9000 konform",
+    "Unser ISP hat (Switching-, Routing-, SMDS-, etc.) Probleme",
+    "Die root-Nameserver sind nicht mehr synchronisiert",
+    "Exzessiver Hochspannungsschutz verhindert eine schnellere Übertragung der Daten",
+    "Der digitale Manipulator überschreitet seine Geschwindigkeitparameter",
+    "Jemand hat Zwergpakete gebroadcastet und der Router wusste nicht mit ihnen umzugehen",
+    "Quanten-Decoherenzen",
+    "Ich würde ihnen gerne helfen, aber die Chefetage hat mir einen Übergestellten Auftrag gegeben",
+    "Es findet gleich eine Besprechung bezüglich der Businessrelevanten-Actionpoints statt",
+    "Mal wieder ein Klassicher BNC (brain not connected) Fehler",
+    "Veraltete Hardware",
+    "Es kam zu einem OSI-Layer 8 Fehler"]
+
+    Ausrede = random.choice(Ausreden_Liste)
+    print ("\n Die Ausrede des Tages lautet - ", Ausrede)
+
+Zufallsgenerator()
